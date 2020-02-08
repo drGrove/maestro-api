27a5840548c4edee0bba9e33ba0476b520d96e58
Danny Grove <danny@drgrovellc.com>
2020-02-08 07:59:07 -0800

    Add SERVER_NAME config to fix url_for generation

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     # General
     ENVIRONMENT = environ.get('ENV') or 'development'
     FLASK_DEBUG = environ.get('FLASK_DEBUG')
+    SERVER_NAME = environ.get('SERVER_NAME') or '127.0.0.1'
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
